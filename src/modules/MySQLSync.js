e99e594a9a815634fa50b91d7a916d4d93a4df53
SrWhale <contato.pauloideal@gmail.com>
2022-11-05 20:06:56 -0300

    Total

--- src/modules/MySQLSync.js
+++ src/modules/MySQLSync.js
@@ -23,11 +23,27 @@ module.exports = class MySQLSync {
                     const newPlayers = players.filter(p => !this.players.includes(p));
 
                     newPlayers.forEach(async player => {
-
-                        const ref = await this.client.database.ref(`RedeLegit/sync/${player}`).once('value').then(res => res.val());
                         const userData = await Util.getMysqlInformation2(this.client, `SELECT * FROM galax.accounts WHERE username = '${player}'`);
 
-                        const metadata = await util.getMysqlInformation2()
+                        if (!userData) return;
+
+                        const metadata = await Util.getMysqlInformation2(this.client, `SELECT * FROM galax.metadata WHERE name = '${player}'`);
+
+                        const vip = await Util.getMysqlInformation2(this.client, `SELECT * FROM galax.group_infos WHERE user = '${userData[0].unique_id}'`);
+
+                        const newUserData = {
+                            uuid: userData.unique_id,
+                            username: userData.username,
+                            group_name: metadata.current_group
+                        };
+
+                        const update = await Util.getMysqlInformation2(this.client, `UPDATE commons.accounts SET uuid = '${newUserData[0].uuid}', username = '${newUserData[0].username}', group_name = '${newUserData[0].group_name}' WHERE username = '${player}'`);
+                        if (update === 'noFound') await Util.getMysqlInformation2(this.client, `INSERT INTO commons.accounts (uuid, username, group_name) VALUES ('${newUserData[0].uuid}', '${newUserData[0].username}', '${newUserData[0].group_name}')`);
+
+                        if (userData.cash > 0) await Util.getMysqlInformation2(this.client, `UPDATE commons.cashing SET cash = '${userData[0].cash}' WHERE username = '${player}'`);
+                        if (userData.cash > 0) await Util.getMysqlInformation2(this.client, `UPDATE galax.accounts SET cash = '0' WHERE username = '${player}'`);
+
+                        if (vip !== "noFound") await Util.getMysqlInformation2(this.client, `UPDATE commons.vips_info SET vip_name = '${vip[0].role}', current_vip = '${vip[0].role}', expire_at = '${String(vip[0].expire_at).split(" ")[0]}' WHERE username = '${player}'`);
                     })
                 })
         }, 10000)
